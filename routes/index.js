69a6d7120d96fbe5135a3206c1978e24363c5c99
davidmiraqyan771-stack <davidmiraqyan771@gmail.com>
2026-05-13 18:37:02 +0400

    js file updated

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import Room from '../models/room.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,12 +10,21 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
-});
+  const filePath = path.join(__dirname, '../views/index.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading index.html');
+    }
+    const ipStr = process.env.ip ? process.env.ip : 'Not set';
+    const modifiedData = data.replace('</body>', `  <div style="text-align: center; color: white;">IP: ${ipStr}</div>\n</body>`);
+    res.send(modifiedData);
+  });
+g});
 
 router.get('/api/rooms', async (req, res) => {
   const rooms = await Room.find().populate('themeId');
   res.json(rooms);
 });
+
 
 export default router;
