fbce5ef2efbe3d2be12ac3becd814cd66d8daf58
davidmiraqyan771-stack <davidmiraqyan771@gmail.com>
2026-05-13 19:30:17 +0400

    js file updated

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,35 @@
-import express from 'express';
-import Room from '../models/room.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
+import express from "express";
+import Room from "../models/room.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const filePath = path.join(__dirname, '../views/index.html');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+router.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "../views/index.html");
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      return res.status(500).send('Error reading index.html');
+      return res.status(500).send("Error reading index.html");
     }
-    const ipStr = process.env.ip ? process.env.ip : 'Not set';
-    const modifiedData = data.replace('</body>', `  <div style="text-align: center; color: white;">IP: ${ipStr}</div>\n</body>`);
+    const ipStr = process.env.ip ? process.env.ip : "Not set";
+    const modifiedData = data.replace(
+      "</body>",
+      `  <div style="text-align: center; color: white;">IP: ${ipStr}</div>\n</body>`,
+    );
     res.send(modifiedData);
   });
 });
 
-router.get('/api/rooms', async (req, res) => {
-  const rooms = await Room.find().populate('themeId');
+router.get("/api/rooms", async (req, res) => {
+  const rooms = await Room.find().populate("themeId");
   res.json(rooms);
 });
-
 
 export default router;
